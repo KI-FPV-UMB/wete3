d6d8b22ca9fd72454c8808e4f28a9f440838d668
msamuelcikova <michaela.samuelcikova@transteq.sk>
2023-02-13 13:25:14 +0100

    cvicenie 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+export enum Menu {
+  BOOKS = 'BOOKS',
+  USERS = 'USERS',
+  BORROWINGS = 'BORROWINGS'
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'wete3';
+
+  form: FormGroup;
+
+  persons: Array<{
+    id: number,
+    name: string;
+    surname: string
+  }> = [];
+
+  menu = Menu;
+
+  actualMenu: Menu = Menu.USERS;
+
+  constructor() {
+    this.form = new FormGroup({
+      id: new FormControl(),
+      name: new FormControl(null, Validators.required),
+      surname: new FormControl(null, [Validators.required, Validators.minLength(3)])
+    })
+  }
+
+  changeMenu(menuItem: Menu): void {
+    this.actualMenu = menuItem;
+  }
+
+  savePerson(): void {
+    if (this.form.controls.id.value) {
+      const index = this.persons.findIndex(person => person.id === this.form.controls.id.value);
+      if (index !== -1) {
+        this.persons[index] = this.form.value;
+      }
+    } else {
+      this.persons.push({
+        id: Date.now(),
+        name: this.form.controls.name.value,
+        surname: this.form.controls.surname.value
+      });
+    }
+    this.form.reset();
+  }
+
+  editPerson(index: number): void {
+    this.form.setValue(this.persons[index]);
+  }
+
+  deletePerson(index: number): void {
+    this.persons.splice(index, 1);
+  }
 }
