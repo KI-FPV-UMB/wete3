881df2241a8b4aca4b50bdc021f5e4e616d7253b
RastislavBiba <83280807+RastislavBiba@users.noreply.github.com>
2023-02-21 00:44:05 +0100

    cvicenie3: Vytvorenie komponentu pre uzivatela, routing na apiUrl/user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import {FormGroup, FormControl} from "@angular/forms";
+import {Component} from '@angular/core';
+
+
 
 export enum Menu {
   BOOKS = 'BOOKS',
@@ -13,27 +14,13 @@ export enum Menu {
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  form: FormGroup;
 
-  persons: Array<{
-    name: string,
-    surname: string
-  }> = [];
 
   menu = Menu;
   actualMenu = Menu.USERS;
 
-  constructor() {
-    this.form = new FormGroup({
-      name: new FormControl(),
-      surname: new FormControl()
-    })
-  }
 
-  savePerson(): void {
-    this.persons.push(this.form.value);
-    this.form.reset();
-  }
+
 
   changeMenu(menuItem: Menu): void {
     this.actualMenu = menuItem;
